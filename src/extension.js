47429ed4c9c4670a9c3341bba0faf3c4f36e8949
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-06-03 17:41:13 +0200

    Updated filetypes on which the extension will run

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,6 @@
 const { commands, window, workspace, WorkspaceEdit, Range } = require("vscode");
 
-const activate = context => {
+exports.activate = function(context) {
   let disposable = commands.registerCommand("extension.rubyReplace", () => {
     if (!window.activeTextEditor) {
       return null;
@@ -9,27 +9,30 @@ const activate = context => {
     const editor = window.activeTextEditor;
     const document = editor.document;
 
-    if (editor && document.languageId === "ruby" || document.languageId === "haml") {
+    if (
+      (editor && document.languageId === "ruby") ||
+      document.languageId === "haml" ||
+      document.languageId === "erb" ||
+      document.languageId === "html.erb"
+    ) {
       const text = document.getText();
       const regex = /:("?(\w+|([\w+]*-[\w+]*)+)"?)\s?=>\s?/g;
 
-      const formatted = text.replace(regex, '$1: ');
-      
+      const formatted = text.replace(regex, "$1: ");
+
       const fullRange = new Range(
-          document.positionAt(0),
-          document.positionAt(text.length)
+        document.positionAt(0),
+        document.positionAt(text.length)
       );
 
-      let edit = new WorkspaceEdit();
+      const edit = new WorkspaceEdit();
       edit.replace(document.uri, fullRange, formatted);
-      window.setStatusBarMessage('Ruby syntax replaced', 3000);
+      window.setStatusBarMessage("Ruby syntax replaced", 3000);
       return workspace.applyEdit(edit);
     } else {
-      window.showErrorMessage('Not a Ruby file');
+      window.showErrorMessage("Not a Ruby file");
     }
   });
 
   context.subscriptions.push(disposable);
 };
-
-exports.activate = activate;
